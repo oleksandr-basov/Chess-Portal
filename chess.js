87ea9d32129a8de96318db33b2d674a73f41957f
Oleksandr Basov <olexander.basov@gmail.com>
2023-06-12 11:05:21 +0300

    Fixed the chessboard layout and piece rendering

--- chess.js
+++ chess.js
@@ -1,16 +1,16 @@
 const chessPieces = {
-    'r': 'images/black_rook.png',
-    'n': 'images/black_knight.png',
-    'b': 'images/black_bishop.png',
-    'q': 'images/black_queen.png',
-    'k': 'images/black_king.png',
-    'p': 'images/black_pawn.png',
-    'R': 'images/white_rook.png',
-    'N': 'images/white_knight.png',
-    'B': 'images/white_bishop.png',
-    'Q': 'images/white_queen.png',
-    'K': 'images/white_king.png',
-    'P': 'images/white_pawn.png',
+    'r': './images/black_rook.png',
+    'n': './images/black_knight.png',
+    'b': './images/black_bishop.png',
+    'q': './images/black_queen.png',
+    'k': './images/black_king.png',
+    'p': './images/black_pawn.png',
+    'R': './images/white_rook.png',
+    'N': './images/white_knight.png',
+    'B': './images/white_bishop.png',
+    'Q': './images/white_queen.png',
+    'K': './images/white_king.png',
+    'P': './images/white_pawn.png',
 };
 
 const initialChessState = [
@@ -33,38 +33,37 @@ function createChessBoard() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             let cell = document.createElement('div');
-            cell.className = 'cell';
+            cell.className = `cell ${((i % 2) ^ (j % 2)) ? 'light' : 'dark'}`;
             cell.id = `cell-${i}-${j}`;
-            if ((i + j) % 2 === 0) {
-                cell.className += ' white';
-            } else {
-                cell.className += ' black';
-            }
+
+            let piece = document.createElement('div');
+            piece.className = 'piece';
+            cell.appendChild(piece);
+
             chessBoard.appendChild(cell);
+
+            // Bind the 'click' event to the cell
+            cell.addEventListener('click', handleCellClick);
         }
     }
-
-    // Bind the 'click' event to all cells
-    chessBoard.querySelectorAll('.cell').forEach((cell) => {
-        cell.addEventListener('click', handleCellClick);
-    });
 }
 
 const handleCellClick = (event) => {
     // Check if a piece is already selected
     if (window.selectedPiece) {
         // Retrieve the destination cell and the selected piece info
-        const destinationCell = event.target;
+        const destinationCell = event.currentTarget;
         const { piece, fromCell } = window.selectedPiece;
 
         // If the destination cell is not empty, do nothing
-        if (destinationCell.style.backgroundImage !== "") {
+        if (destinationCell.querySelector('.piece').style.backgroundImage.includes('url')) {
             return;
         }
 
         // Move the piece from the old cell to the new cell
-        fromCell.style.backgroundImage = '';
-        destinationCell.style.backgroundImage = piece;
+        fromCell.querySelector('.piece').style.backgroundImage = '';
+        destinationCell.querySelector('.piece').style.backgroundImage = piece;
+        destinationCell.querySelector('.piece').style.backgroundSize = 'contain';  // Move the piece style
 
         // Remove the 'selected' class from the old cell
         fromCell.classList.remove('selected');
@@ -72,16 +71,15 @@ const handleCellClick = (event) => {
         // Reset window.selectedPiece
         window.selectedPiece = null;
     } else {
-        // Select a piece
-        const selectedCell = event.target;
-        const selectedPiece = selectedCell.style.backgroundImage;
+        // Select a cell
+        const selectedCell = event.currentTarget;
+        const selectedPiece = selectedCell.querySelector('.piece').style.backgroundImage;
 
         // Check if a piece is selected
-        if (selectedPiece !== "") {
+        if (selectedPiece.includes('url')) {
             // Add 'selected' class to highlight the cell
             selectedCell.classList.add('selected');
 
-            // Save
             // Save the selected piece and cell for later use
             window.selectedPiece = {
                 piece: selectedPiece,
@@ -94,11 +92,12 @@ const handleCellClick = (event) => {
 function renderChessPieces(chessState) {
     for (let i = 0; i < chessState.length; i++) {
         for (let j = 0; j < chessState[i].length; j++) {
-            let piece = chessState[i][j];
-            if (piece !== ' ') {
-                let cell = document.getElementById(`cell-${i}-${j}`);
-                cell.style.backgroundImage = `url(${chessPieces[piece]})`;
-                cell.style.backgroundSize = 'contain';  // Fit image within cell
+            let pieceCode = chessState[i][j];
+            if (pieceCode !== ' ') {
+                let piece = document.querySelector(`#cell-${i}-${j} .piece`);
+                piece.style.backgroundImage = `url(${chessPieces[pieceCode]})`;
+                piece.style.backgroundSize = 'contain';  // Fit image within cell
+                piece.style.backgroundRepeat = 'no-repeat';
             }
         }
     }
